d93d936e6eaec57e440505967c9c30d593fe8a3a
chap <chaplonglau@gmail.com>
2018-01-11 22:20:35 -0800

    test to see json

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+
+function topRated(){
+  $("#top-rated").click(function(){
+    list=JSON.parse(jsonString);
+    debugger
+  })
+}
+
+
+
+$(document).ready(function(){
+  topRated()
+});
+
+
